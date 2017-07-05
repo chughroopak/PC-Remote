922a85a84119de048c2452ff4f5810b3d34dac2c
Roopak Chugh <chughroopak@gmail.com>
2017-07-06 00:40:11 +0530

    Update PCRemoteUI.java

--- Startup/PCRemoteUI.java
+++ Startup/PCRemoteUI.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Startup;
 
 import Server.ServerInit;
 
-/**
- *
- * @author dell pc
- */
 public class PCRemoteUI extends javax.swing.JFrame {
 
     /**
