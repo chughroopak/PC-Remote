4b615f3655f9ecd185eac07237c614ca90a56b9e
Archit Garg <architgarg32@gmail.com>
2017-07-15 15:47:49 +0530

    CDrawInit.java

---
+++ CDrawInit.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chatexpress;
+
+import Chatexpress.CDrawGUI;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Archit Garg
+ */
+public class CDrawInit extends Thread{
+    String server_ip = null; 
+    CDrawGUI dg;
+    
+    public CDrawInit(String server_ip) {
+        this.server_ip=server_ip;
+        dg=new CDrawGUI(server_ip);
+        dg.setVisible(true);
+        start();
+        }
+    
+    public void run(){
+            try {
+                
+                 do{
+                     
+                String smessage=dg.dis.readUTF();
+                dg.jTextArea1.append("server:"+smessage+"\n");
+                     
+                     }while(true);
+            } catch (IOException ex) {
+                Logger.getLogger(CDrawInit.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        
+    }
+}
