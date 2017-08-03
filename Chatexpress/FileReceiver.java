fcd91903ba7d1bfd033c99b86c71caf7760df544
Archit Garg <architgarg32@gmail.com>
2017-08-03 11:50:06 +0530

    Update FileReceiver.java

--- Chatexpress/FileReceiver.java
+++ Chatexpress/FileReceiver.java
@@ -19,7 +19,12 @@ public class FileReceiver extends Thread {
     PrintWriter pw;
      JFrame f;
     
-    
+    /*
+    *parameterised constructor
+    *set connection with server file transfer window
+    *create file according to name and path sent by server file transfer window
+    *starts the thread
+    */
     public FileReceiver(String server_ip) throws IOException{
         try{
             this.server_ip=server_ip;
@@ -46,6 +51,9 @@ public class FileReceiver extends Thread {
         }
     }
     
+    /*
+    *receive file data from server side and store it 
+    */
     public void run(){
         String str="";
                 try{
